8c649794ffda10e2a68a46cf553e31d99b08bb08
chm2062 <chm2062@med.cornell.edu>
2018-09-14 16:38:32 -0700

    first code edit 3!

--- demean_age.py
+++ demean_age.py
@@ -1,5 +1,5 @@
 import numpy as np
 age = np.loadtxt('participants.tsv', skiprows=1, usecols=1)
 meanage = sum(age)/len(age)
-np.savetxt("demeaned_age.txt", age-mean_age)
+np.savetxt("demeaned_age.txt", age-meanage)
 print("done!")
